f9f10d0e88651df4d6755ee7081d35947f60ba00
T3G1 <t3g1@mifort.org>
2016-12-22 18:35:46 +0300

    title added to getProposal

--- backend/client.js
+++ backend/client.js
@@ -102,7 +102,7 @@ exports.getProposal = function(req, res, next){
                     if (clients.find(function(client){return client.clientId == req.user.id})){
                         hiddenText = ', proposals.hiddenText';
                     }
-                    connection.query('SELECT id, description, price, startTime, endTime, category, region, clientId, inProgress' + hiddenText + ' FROM proposals WHERE id = ?',
+                    connection.query('SELECT id, title, description, price, startTime, endTime, category, region, clientId, inProgress' + hiddenText + ' FROM proposals WHERE id = ?',
                         req.params.id, function (err, proposal) {
                         if (err) {
                             logger.error(err);
