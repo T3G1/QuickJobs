a016c668bef73d2ee36a9867d74d2a2ebc6bba9c
IrinaAgamyan <agamyan.irina@gmail.com>
2016-12-22 06:43:02 +0300

    add datepicker, landing page styles

--- frontend/landing/landingController.js
+++ frontend/landing/landingController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('quickJobs.landing', ['ngRoute'])
+angular.module('quickJobs.landing', ['ngRoute', 'ADM-dateTimePicker'])
 
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/landing', {
@@ -9,7 +9,34 @@ angular.module('quickJobs.landing', ['ngRoute'])
         });
     }])
 
-    .controller('landingController', ['$scope', 'landingService',
-        function($scope, landingService) {
+    .controller('landingController', ['$scope', 'landingService', '$http',
+        function($scope, landingService, $http) {
             console.log('landing');
+            $scope.user = {email: undefined, pass: undefined};
+            $scope.prop = {};
+            $scope.sendProposal = function () {
+                console.log ('sendProposal');
+            };
+
+            $scope.logInd = function () {
+
+            };
+
+            $scope.signUp = function () {
+                var data = {'email': $scope.user.email, 'password': $scope.user.pass};
+                var req = {
+                    method: 'PUT',
+                    url: '/api/client/signup',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    data: data
+                }
+                $http(req).then(function() {
+                    console.log('+');
+                }, function(err) {
+                    console.log ('err');
+                    console.log (err);
+                });
+            }
         }]);
